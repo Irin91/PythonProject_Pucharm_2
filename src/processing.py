4e38ca7697ab8614ccd93fff8c9bd5f8dc285245
iring <iringubina1991@gmail.com>
2025-04-15 01:00:34 +0300

    changes processing.py

--- src/processing.py
+++ src/processing.py
@@ -10,9 +10,13 @@ def filter_by_state(operations: List[Dict[str, Any]], state: str = "EXECUTED") -
 def sort_by_date(operations: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
     """Функция принимает список словарей и параметр, задающий порядок сортировки (по умолчанию — убывание)"""
 
-    return sorted(operations, key=lambda item: item["date"], reverse = True)
+    return sorted(operations, key=lambda item: item["date"], reverse=True)
 
 
-print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]))
+print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                       {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]))
 
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]))
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]))
